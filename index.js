a17e7d93a652b54c79e8ba770511d21f1b8516e5
sepmein <sepmein@hotmail.com>
2016-09-21 16:59:43 +0800

    added some basic functions

--- index.js
+++ index.js
@@ -3,13 +3,30 @@
 let authServer = require('userjs');
 let app = require('koa');
 let router = require('koa-router');
-let db = require('mongog');
-let allowMethods = require('koa-allow-methods');
+let Mongo = require('mongodb');
 let jwt = require('json-web-token');
+let cors = require('koa-cors');
+let allowMethods = require('koa-allow-methods');
+let parseBody = require('./middlewares/parseBody');
 
 // configuration
 const CONF = require('./configuration');
+const COLLECTION = CONF.main.collection;
+const PORT = CONF.main.port;
+const AUTH = CONF.auth;
+authServer.run(CONF.auth);
 
-authServer.run(CONF);
+app.use(cors());
+app.use(allowMethods(['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS']));
+app.use(parseBody);
+app.use(router.routes());
 
-app.use()
+Mongo.connect('mongodb://local:27017/' + COLLECTION, (err, db) => {
+    if (err) {
+        app.throw(500);
+    }
+    app.context.CONFIGURATION = CONF;
+    app.context.db = db;
+    app.context.collection = db.collection(COLLECTION);
+    app.listen(PORT);
+});
