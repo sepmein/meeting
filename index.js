bc82612b005bbc2961396c51bc645f02ce5b9b7f
Spencer Zhang <sepmein@gmail.com>
2016-09-21 19:36:41 +0800

    added some imports

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // start authentication server
 let authServer = require('userjs');
 authServer(require('./config.js').auth);
+
+import koa from 'koa';
+import router from 'koa-router';
+import cors from 'koa-cors';
+import mongodb from 'mongodb';
+import cobody from 'co-body';
+
+let app = koa();
